23e721990e5c487697253e0e17c08d2dc954ad1c
Garrett Gregor <garrett.gregor@gmail.com>
2024-02-12 22:13:15 -0700

    fix faker email

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    email { Faker::Interner.email }
+    email { Faker::Internet.email }
     photo_url { Faker::Placeholdit.image }
     is_admin { Faker::Boolean.boolean }
   end
